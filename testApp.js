9d5409c8dc6cb7bad56f24493ef833cacdfd80c0
Bondifrench <d_dumaine@yahoo.com>
2014-04-07 23:45:19 +1000

    added first controller

--- testApp.js
+++ testApp.js
@@ -1,14 +1,29 @@
 #!/usr/bin/env node
 "use strict";
 
+/**
+ * Module dependencies.
+ */
+
 var express = require("express");
 var app = express();
+var path = require("path");
 
+// all environments
 var port = process.env.PORT || 8080;
-//app.use(express.logger());
+
+app.use(express.logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// development only
+if ("development" == app.get("env")) {
+  app.use(express.errorHandler());
+}
+
 app.get("/", function(req, res){
   res.sendfile("./index.html");
 });
+
 
 app.listen(port, function () {
 	console.log("Listening on port: " + port);
