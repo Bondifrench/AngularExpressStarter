55389c93677d742553c6885e62e99e14b95793bd
Bondifrench <d_dumaine@yahoo.com>
2014-04-07 18:12:37 +1000

    First steps in Express and Angular

---
+++ testApp.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+"use strict";
+
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 8080;
+//app.use(express.logger());
+app.get("/", function(req, res){
+  res.sendfile("./index.html");
+});
+
+app.listen(port, function () {
+	console.log("Listening on port: " + port);
+});
